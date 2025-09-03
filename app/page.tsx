db73a7580aecc1d81183232804a256a0b203ea40
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-09-03 21:50:24 +0600

    Add React Query integration and wrap children with Providers component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
+
+function fetchUsers() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+    res.json()
+  );
+}
 
 export default function Home() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  if (data) {
+    console.log("Fetched users:", data);
+  }
   return (
     <div className="flex flex-col  justify-center ">
       <main>Hello User Dashboard</main>
