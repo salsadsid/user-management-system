7cca3672db185c778ff47626665b9d963fd7bc12
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-09-03 21:35:03 +0600

    install shadcn and configure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col  justify-center ">
       <main>Hello User Dashboard</main>
+      <Button>Search</Button>
     </div>
   );
 }
